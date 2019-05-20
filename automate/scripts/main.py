dbe9cb90cdebf3f7c1ab0c7ea0f4e690fc0d075b
CharlesQlara <charles@qlara.fr>
2019-05-20 11:18:04 +0200

    refactored automate/init.py

--- automate/scripts/main.py
+++ automate/scripts/main.py
@@ -1,8 +1,55 @@
-from time import sleep
 import os
+import calendar
+import time
+import random
+import json
+import socket
 
 while True:
-    os.system("python automated.py")
-    sleep(10)
-    os.system("python automateClient.py")
-    sleep(50)
+    # Hostname doit etre "id_unite"
+    unit_id = 1  # pour le moment, sinon (socket.gethostname())[0].encode('utf-8')
+    types = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
+    data = {}
+    date_str = str(calendar.timegm(time.gmtime()))
+
+    # generation du nom du fichier
+    f = "paramunite_" + str(unit_id) + "_" + date_str + ".json"
+
+    with open(f, "w+") as outfile:
+        i = 1
+
+        array = []
+        while i < 11:
+            data = {
+                       'unit_id': unit_id,
+                       'automaton_number': i,
+                       'automaton_type': '0X000BA2' + str(random.choice(types)),
+                       'tank_temp': round(random.uniform(2.5, 4.0), 1),
+                       'external_temp': round(random.uniform(8.0, 14.0), 1),
+                       'milk_weight_tank': random.randrange(3512, 4607, 1),
+                       'finished_product_weight': 0,
+                       'ph_measurement': round(random.uniform(6.8, 7.2), 1),
+                       'k_measurement': random.randrange(35, 47, 1),
+                       'nacl_concentration': round(random.uniform(1.0, 1.7), 1),
+                       'lvl_bact_salmo': random.randrange(17, 37, 1),
+                       'lvl_bact_ecoli': random.randrange(35, 49, 1),
+                       'lvl_bact_listeria': random.randrange(28, 54, 1),
+                       'epoch': date_str
+                   },
+            i = i + 1
+            array.append(data)
+
+        os.environ["FILE_NAME"] = f
+        json.dump(array, outfile, indent=1)
+    time.sleep(10)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("", 1111))
+
+    f = os.environ["FILE_NAME"]
+    with open(f, 'rb') as _file:
+        s.send(_file.read())
+
+    print("Le fichier a ete correctement ete envoye au serveur : %s." % f)
+
+    s.recv(2048)
+    time.sleep(50)
