070a566ae71ad5ddf0adeb8cc7ac1fa971f46e2c
CharlesQlara <charles@qlara.fr>
2019-05-27 14:41:16 +0200

    renamed table name

--- webserver/routes/automatons.js
+++ webserver/routes/automatons.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 router.get('/getBy', (req, res) => {
     console.log(req.query.search)
-    let query = "SELECT * FROM `automatons` where unit_id = "+ req.query.search+" ORDER BY epoch LIMIT 600;"; // get automatons by unit_id
+    let query = "SELECT * FROM `automaton` where unit_id = "+ req.query.search+" ORDER BY epoch LIMIT 600;"; // get automatons by unit_id
         // execute
         db.query(query, (err, result)=>{
             if (err) {
