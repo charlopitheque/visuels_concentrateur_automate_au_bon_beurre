c8c43efa44f7ba355ff20d56f7012068066810dc
CharlesQlara <charles@qlara.fr>
2019-07-01 13:08:12 +0200

    searching for the good ip

--- webserver/assets/static/js/api/config.mjs
+++ webserver/assets/static/js/api/config.mjs
@@ -1,5 +1,5 @@
 export default {
     //BASE_URL : "http://192.168.152.152:5000",
-    BASE_URL : "http://127.0.0.1:5000",
+    BASE_URL : "http://18.191.105.228:5000",
     AUTOMATONS_URL : '/automatons',
 }
