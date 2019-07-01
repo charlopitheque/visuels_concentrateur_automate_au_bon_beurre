764c26091fbee8eacde5bbe66c4395a945c33179
CharlesQlara <charles@qlara.fr>
2019-07-01 13:10:49 +0200

    searching for the good ip

--- webserver/routes/automatons.js
+++ webserver/routes/automatons.js
@@ -3,7 +3,7 @@ let router = express.Router();
 router.get('/getBy', (req, res) => {
     console.log(req.query.search)
 
-    let query = "SELECT * FROM `AUTOMATON` where CD_UNITE = "+ req.query.search+" ORDER BY id DESC LIMIT "+req.query.limit+";";
+    let query = "SELECT * FROM `AUTOMATON` where CD_UNIT = "+ req.query.search+" ORDER BY id DESC LIMIT "+req.query.limit+";";
     // get automatons by unit_id  
     // execute
         db.query(query, (err, result)=>{
