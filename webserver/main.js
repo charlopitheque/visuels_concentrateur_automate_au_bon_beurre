a929825cca4e908cc7f91524e451f059c36cc316
CharlesQlara <charles@qlara.fr>
2019-07-01 11:20:13 +0200

    uopdate bdd user in frontend

--- webserver/main.js
+++ webserver/main.js
@@ -14,8 +14,8 @@ const port = 5000;
 const db = mysql.createConnection (
     {
         host: '127.0.0.1',
-        user: 'admin',
-        password: 'admin',
+        user: 'datavision',
+        password: 'datavision',
         database: 'BDD_USINE_AU_BON_BEURRE'
     }
 );
